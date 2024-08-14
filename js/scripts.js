9dfd9db6f6bd4a5e932b768a653713bd826d1623
Daniel JR <daniel.hemp92@gmail.com>
2024-08-14 15:09:16 -0300

    add certificados

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,35 @@ function exibirIdade() {
     exibirIdade();
 
   setInterval(exibirIdade, 86400000); // Atualiza a cada 24 horas
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Seleciona o modal e a imagem dentro dele
+    var modal = document.getElementById("dynamicModal");
+    var modalImage = document.getElementById("modalImage");
+    var closeModal = document.querySelector(".close");
+
+    // Seleciona todos os botões que abrem o modal
+    var buttons = document.querySelectorAll(".openModalBtn");
+
+    // Adiciona um evento de clique para cada botão
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var certSrc = this.getAttribute("data-cert");
+            modalImage.src = certSrc;
+            modal.style.display = "flex";
+        });
+    });
+
+    // Fecha o modal ao clicar no "x"
+    closeModal.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    // Fecha o modal ao clicar fora do conteúdo
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
+
